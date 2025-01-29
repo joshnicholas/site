bbbf8fe28f103a5321a2feff7a14a79349806853
Josh Nicholas <hi@thambi.li>
2025-01-30 09:22:09 +1100

    Text changes

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -33,13 +33,13 @@
 
       <p class=''>Ello.</p><br>
       <!-- , <a href='https://joshnicholas.com/projects'>projects</a> I'm working on -->
-      <p>My name is <span class="font-semibold">Josh</span> and I'm a scribbler and <a href='https://www.theguardian.com/profile/josh-nicholas' target='_blank' rel="me">journalist</a>.</p><br>
+      <p>My name is <span class="font-semibold">Josh</span>. I'm a scribbler and <a href='https://www.theguardian.com/profile/josh-nicholas' target='_blank' rel="me">journalist</a>.</p><br>
 
-      <p>Welcome. This site is an experiment and a work in progress. Sorry if anything breaks.</p><br>
+      <p>This site is an experiment and I'm constantly tinkering. Sorry if anything breaks while you're here.</p><br>
         
-      <p>For almost 20 years you would have found a much neglected blog at this domain. The book reviews I posted in high school were truly terrible. In 2019 I <a href='https://www.theguardian.com/lifeandstyle/2022/aug/15/i-used-to-think-art-required-natural-talent-then-i-taught-myself-to-draw'>started to take drawing a bit more seriously</a> and began posting scribbles semi-regularly.</p><br>
+      <p>For almost 20 years you would have found a much neglected blog at this domain. The book reviews I posted in high school were truly terrible. In 2019 I <a href='https://www.theguardian.com/lifeandstyle/2022/aug/15/i-used-to-think-art-required-natural-talent-then-i-taught-myself-to-draw'>started to take drawing a bit more seriously</a> and began posting them semi-regularly.</p><br>
 
-      <p>But I got sick of seeing my scribbles spread across different platforms, and lost in chronological timelines. This new site is an attempt to bring everything together and make it more discoverable. I'll run a script to update about once a week.</p><br>
+      <p>Eventually I got sick of seeing my scribbles spread across different platforms, and lost in chronological timelines. This new site is an attempt to bring everything together and make it more discoverable. I'll run a script to update about once a week.</p><br>
 
       </div>
 
