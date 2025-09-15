bd79c58c862af19566e6cd82433f0b1b00deb3c3
Josh Nicholas <hi@thambi.li>
2025-09-15 20:31:04 +1000

    center nav

--- src/lib/components/nav.svelte
+++ src/lib/components/nav.svelte
@@ -4,8 +4,9 @@
     let csso = upDown.toLowerCase() == 'down' ? 'border-top: 2px solid;margin-top:1.25rem;padding-top:0.5rem;display:inline-block;' : 'border-bottom: 2px solid;margin-bottom:1.25rem;padding-bottom:0.5rem;display:inline-block;'
 </script>
 
-<div id='navblock' style={csso}>
-    <ul id='navynav' class='space-x-6 flex justify-start text-l font-semibold'>
+<div class='text-center'>
+<div id='navblock' class='block' style={csso}>
+    <ul id='navynav' class='space-x-6 flex justify-center text-l font-semibold'>
         <li class="mr-0">
             <a href='/'>scribbles</a>
         </li >
@@ -25,6 +26,7 @@
             <a href='https://www.instagram.com/joshcnicholas' target='_blank' rel="me">insta⤴</a>
         </li> -->
     </ul>
+</div>
 </div>
 
 <style>
