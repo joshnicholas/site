3c9dc066cc363ff3967cf34cf5f4c3006b75c351
Josh Nicholas <hi@thambi.li>
2026-05-26 19:25:26 +1000

    update favs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -50,15 +50,18 @@ let feature = ["240711_last-full-day_0.jpg","240714_escaping-the-hu_0.jpg","2401
         '231008_streetball.jpeg','240124-sydney-ferry.jpg','purple_head.jpg',
         '230613-ipoh-french-patisserie.jpeg', '230621-penang-town-hall.jpeg',
         '230623-the-hotel.jpeg','240726_food-here-has_0.jpg','240710_had-another-crack_0.jpg',
-        'bafkreiebe4cdzj5eoovpgpf25yri6a3gcbe3pz4bepvd6ibquy22ihyb2u.jpg','bafkreielepiii3lhuzunv6bhhrgc3kstq2ta7azgks7bywkf3w27gu7ine.jpg',
+        'bafkreielepiii3lhuzunv6bhhrgc3kstq2ta7azgks7bywkf3w27gu7ine.jpg',
         'bafkreiamgekie2fpvpehuynwfaic3ck5jbvnubebls37gmivgnrcew2mdu.jpg','bafkreib64zsamf3z2s23bmrigo457riaqgpzj7zvihwbwjl5yvftljo6zq.jpg',
         'bafkreicmocizke57lx7km6s3jgq22bzyuwsnaplbqrsa5crgdykctmxeza.jpg',
         'bafkreictzaz4oeshhzwuyzbz55qtieuopnatijpqbknebn5nfsycyxaqry.jpg','bafkreickngshyrlldojxnacpksidqihsmb4misoyecwauwpeckjttml6zi.jpg',
         'bafkreidcm772nxcggw75zkao4rvgdyg5s4or3227ijbkqv3utlt4a7fkqy.jpg','bafkreidpxkyafq2q37ae66tumqdgyiymzxyipsk2amrq4gx2jphdg7ti5i.jpg',
         'bafkreig7mwkri7cii4lsf2d7b5y45ricnpyqsleun2p4tbjs3zwpxs6j7a.jpg','bafkreihhzksklqyyz5dh444fwwuvbm5shpaaqyma7tszc43e4ji4jcgbiu.jpg',
-        'bafkreifjqcy2tfifsiasvvfp5ti5mvdjiueuvov247xnbi5bkkc6abupwa.jpg'
+        'bafkreifjqcy2tfifsiasvvfp5ti5mvdjiueuvov247xnbi5bkkc6abupwa.jpg',
+        'oz45njtul6s74csqfx69qqwfvyn90dost48ta9yx6wv58wtr3w4mrzfa1vu.jpg',
 
         ]
+
+        // http://localhost:5173/images/bafkreiebe4cdzj5eoovpgpf25yri6a3gcbe3pz4bepvd6ibquy22ihyb2u.webp
 
 // Filter out removed images first
 let filteredData = $derived(data.data.filter(item => !removed.includes(item.img_path)))
